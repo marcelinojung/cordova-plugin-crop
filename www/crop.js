d2673257b64f9859f7721b45f8e09349ad6bde3f
Jaroslav Řehořka <jaroslav.rehorka@q2.cz>
2018-01-08 16:57:19 +0100

    default values fix

--- www/crop.js
+++ www/crop.js
@@ -5,8 +5,8 @@ var crop = module.exports = function cropImage (success, fail, image, options) {
   options.targetWidth = options.targetWidth || -1
   options.targetHeight = options.targetHeight || -1
   options.allowRotate = options.allowRotate || false
-  options.keepCropAspectRatio = options.keepCropAspectRatio || true
-  options.showCropGrid = options.showCropGrid || true
+  options.keepCropAspectRatio = !!options.keepCropAspectRatio
+  options.showCropGrid = !!options.showCropGrid
   options.toolbarTitle = options.toolbarTitle || ""
   return cordova.exec(success, fail, 'CropPlugin', 'cropImage', [image, options])
 }
